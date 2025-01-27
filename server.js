9db38f82dcb9ea8d8f859edf3726e52392054998
qotibaeiad <qotibaeiad98@gmail.com>
2025-01-27 11:56:18 +0200

    change port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const nodemailer = require('nodemailer');  // For sending emails
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
